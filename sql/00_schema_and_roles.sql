83005ea3b2b852500a7e671d3967a6c74d758c01
Zach Phillips <zphillips.de@gmail.com>
2025-09-20 23:24:01 -0400

    Created 00_schema_and_roles.sql. Created group roles and zach and bi_tool users.

---
+++ sql/00_schema_and_roles.sql
@@ -0,0 +1,20 @@
+-- **Create Roles**
+-- Group Roles
+CREATE ROLE ems_admin WITH NOLOGIN;
+CREATE ROLE ems_owner WITH NOLOGIN;
+CREATE ROLE etl_writer WITH NOLOGIN;
+CREATE ROLE bi_reader WITH NOLOGIN;
+
+-- User Roles
+CREATE USER zach WITH NOLOGIN;
+GRANT etl_writer TO zach;
+CREATE USER bi_tool WITH NOLOGIN;
+GRANT bi_reader TO bi_tool;
+
+-- **Create Schemas**
+CREATE SCHEMA IF NOT EXISTS etl;
+ALTER SCHEMA etl OWNER TO ems_owner;
+CREATE SCHEMA IF NOT EXISTS stage;
+ALTER SCHEMA stage OWNER TO ems_owner;
+CREATE SCHEMA IF NOT EXISTS mart;
+ALTER SCHEMA mart OWNER TO ems_owner;
