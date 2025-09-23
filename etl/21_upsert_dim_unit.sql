58526db331b6f2f96d094d11a88c5e8efa0b3269
Zach Phillips EMS <zphillips@docogonet.com>
2025-09-23 09:53:13 -0400

    Created 21_upsert_dim_unit.

---
+++ etl/21_upsert_dim_unit.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+WITH src AS (
+    SELECT DISTINCT UPPER(BTRIM(unit_code)) AS code
+    FROM stage.incidents_stg
+    WHERE unit_code IS NOT NULL
+),
+prepared AS (
+    SELECT
+        code AS unit_code
+        CASE
+            WHEN code IN ('EMS1','EMS10')   THEN 'ALS'
+            WHEN code LIKE 'P%'             THEN 'ALS'
+            WHEN code LIKE 'A%'             THEN 'BLS'
+            ELSE 'UNKNOWN'
+        END AS service_level
+    FROM src
+)
+
+INSERT INTO mart.dim_unit (unit_code, service_level)
+SELECT unit_code, service_level
+FROM perpared
+ON CONFLICT (unit_code) DO UPDATE
+SET service_level = EXCLUDED.service_level;
+
+COMMIT;
