24cf932c048149228a330ae97662a8e752e69742
Zach Phillips EMS <zphillips@docogonet.com>
2025-09-23 10:04:09 -0400

    Finished 21_upsert_dim_unit.

--- etl/21_upsert_dim_unit.sql
+++ etl/21_upsert_dim_unit.sql
@@ -1,25 +1,24 @@
 BEGIN;
 
 WITH src AS (
-    SELECT DISTINCT UPPER(BTRIM(unit_code)) AS code
-    FROM stage.incidents_stg
-    WHERE unit_code IS NOT NULL
+  SELECT DISTINCT UPPER(BTRIM(unit_code)) AS code
+  FROM stage.incidents_stg
+  WHERE unit_code IS NOT NULL
 ),
 prepared AS (
-    SELECT
-        code AS unit_code
-        CASE
-            WHEN code IN ('EMS1','EMS10')   THEN 'ALS'
-            WHEN code LIKE 'P%'             THEN 'ALS'
-            WHEN code LIKE 'A%'             THEN 'BLS'
-            ELSE 'UNKNOWN'
-        END AS service_level
-    FROM src
+  SELECT
+    code AS unit_code,
+    CASE
+      WHEN code IN ('EMS1','EMS10') THEN 'ALS'
+      WHEN code LIKE 'P%'           THEN 'ALS'
+      WHEN code LIKE 'A%'           THEN 'BLS'
+      ELSE 'UNKNOWN'
+    END AS service_level
+  FROM src
 )
-
 INSERT INTO mart.dim_unit (unit_code, service_level)
 SELECT unit_code, service_level
-FROM perpared
+FROM prepared
 ON CONFLICT (unit_code) DO UPDATE
 SET service_level = EXCLUDED.service_level;
 
