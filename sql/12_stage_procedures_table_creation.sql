ce7533069fc742d5fed61960f79480991b16c481
Zach Phillips EMS <zphillips@docogonet.com>
2025-10-10 13:09:49 -0400

    Formatting.

--- sql/12_stage_procedures_table_creation.sql
+++ sql/12_stage_procedures_table_creation.sql
@@ -2,9 +2,12 @@ SET ROLE ems_owner;
 BEGIN;
 
 CREATE TABLE IF NOT EXISTS stage.procedures_stg (
-    pcr_number                  TEXT,                           a-- key
+    -- Keys
+    pcr_number                  TEXT,
     procedure_time              TIMESTAMP WITHOUT TIME ZONE,    -- eProcedures.01
-    prior_to_ems                TEXT,                           -- eProcedures.02
+    last_modified               TIMESTAMP WITHOUT TIME ZONE,
+
+    -- Procedure
     procedure_description       TEXT,                           -- eProcedures.03
     procedure_code              INTEGER,                        -- eProcedures.03
     equipment_size              TEXT,                           -- eProcedures.04
@@ -12,10 +15,17 @@ CREATE TABLE IF NOT EXISTS stage.procedures_stg (
     successful                  TEXT,                           -- eProcedures.06
     complication_list           TEXT,                           -- eProcedures.07
     patient_response            TEXT,                           -- eProcedures.08
-    crew_member_id              INTEGER,                        -- eProcedures.09
-    performer_role              TEXT,                           -- eProcedures.10
     authorization               TEXT,                           -- eProcedures.11
     authorizing_physician       TEXT,                           -- eProcedures.12
     vascular_access_location    TEXT,                           -- eProcedures.13
-    last_modified               TIMESTAMP WITHOUT TIME ZONE
+    
+    -- Crew
+    prior_to_ems                TEXT,                           -- eProcedures.02
+    crew_member_id              INTEGER,                        -- eProcedures.09
+    performer_role              TEXT                            -- eProcedures.10
 );
+
+
+
+COMMIT;
+RESET ROLE;
